9aba6f2bcc645a56834fa63ef1abd59cf754edd9
mario <moraorviz@gmail.com>
2019-06-18 23:53:41 +0200

    cors headers

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // debemos poner todas las headers que se aceptan. Content-Type , token
+    next();
+});
+
 var bodyParser = require('body-parser');
 var mongo = require('mongodb')
 var crypto = require('crypto');
